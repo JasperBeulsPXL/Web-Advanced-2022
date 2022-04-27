d509f2455e9dc699e0911ca3930337c2ddd3ad98
Jasper Beuls <jasper.beuls@pxl.be>
2022-04-27 14:38:59 +0200

    Added vue project for DEF

---
+++ ABC/vue-abc/src/components/BuildingItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>vaste waarde</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BuildingItem",
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
